0e972a4c3b4086aad8564f6924a8e6e2aa5df882
Samyak Bhuta <samyak.bhuta@gmail.com>
2014-06-16 18:18:30 +0530

    Major Refactoring
    - Removes methods for getting start, end and size of the block.
    - Adds generic method to add subset for the script block. This also
      adds,
    	- Method to enlist charcodes of the subset
    	- Method to enlist characters of the subset
    	- Method to check if the charcode/character belongs to the subset
    	- One can define subset as an array literal or as a function
    	  returning an array
    - Removes hardcoded method such as 'listAllCodes' etc, as they can be
      now supported with generic method to add subset.
    - Adds largest subset, i.e., entire block, in the constructor itself.
      Name of this subset is human readable script name, i.e., 'Gujarati'.

--- lib.js
+++ lib.js
@@ -1,22 +1,22 @@
 var $gu = function () {
-	this.charCodeStart = 2688; // 2688, start of Gujarati Unicode Code Block
-	this.blockSize = 128; // 128, length of Gujarati Unicode Block
-	this.scriptName = 'Gujarati'; // Human readable name of the Script
+	this.start = 2688; // 2688, start of Gujarati Unicode Code Block
+	this.size = 128; // 128, length of Gujarati Unicode Block
+	this.end = this.start + this.size - 1;
+	this.scriptName = 'Gujarati'; // Human readable name of the script
+
+	// Adding the 'complete' subset !
+	this.addSubset( this.scriptName, this.charcodes( this.start, this.end ) );
 }
 
-$gu.prototype.start = function () {
-	return this.charCodeStart;
+$gu.prototype.charcodes = function ( start, end ) {
+	var list = [];
+	for ( var i = start ; i <= end ; i++ ) {
+		list.push(i);
+	}
+	return list;
 }
 
-$gu.prototype.size = function () {
-	return this.blockSize;
-}
-
-$gu.prototype.end = function () {
-	return this.start() + this.size() - 1;
-}
-
-// map the code points to actual chars.
+// map the code points to actual chars
 // e.g [2709,2710,2710] gives ['ક','ખ','ગ']
 $gu.prototype.toChars = function ( charCodes ) {
 	return charCodes.map( function (i) {
@@ -24,26 +24,38 @@ $gu.prototype.toChars = function ( charCodes ) {
 	});
 }
 
-// Entire Gujarati Unicode Range as codepoint values.
-// Use ```toChars``` method to get the characters out of it.
-$gu.prototype.listAllCodes = function () {
-	var list = [];
-	for ( var i = this.start() ; i <= this.end() ; i++ ) {
-		list.push(i);
-	}
-	return list;
-}
+// Adds a named subset to ```$gu``` with common methods
+// Provide subset name and members (as charcodes!) of the subset as an array
+$gu.prototype.addSubset = function ( name, members ) {
 
-$gu.prototype.listAll = function () {
-	return this.toChars( this.listAllCodes() );
-}
+	// if ```members``` is a function, get individual members as array
+	members =  ( typeof members === 'function' ) ? members() : members;
 
-$gu.prototype.isInRange = function ( char ) {
-	char =  typeof char === 'string' ? char.charCodeAt(0) : char ;
-	if ( typeof char === 'number' && this.start() <= char && char <= this.end() ) {
-		return true;
+	// if ```members``` is not resolved as an array, then quit
+	if ( !Array.isArray(members) )
+		return null;
+
+	// Creating ```get<Subset>CodeList``` method
+	// Will return all the members as charcodes
+	this[ 'get' + name + 'CodeList' ] = function () {
+		return members;
 	}
-	return false;
+
+	// Creating ```get<Subset>List``` method
+	// Will return all the members as chars
+	this[ 'get' + name + 'List' ] = function () {
+		return this.toChars( members );
+	}
+
+	// One can pass charcode or the char itself
+	// Uses indexOf to check is a charcode or char belongs to a subset of not
+	this[ 'is' + name ] = function ( char ) {
+		char = ( typeof char === 'string' ) ? char.charCodeAt(0) : char ;
+		return members.indexOf(char);
+	}
+
+	// things went smoothly, all the helper methods should be available
+	return true;
 }
 
 module.exports = new $gu();
